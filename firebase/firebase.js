d270a484eb8b4982c1747c3669419ad36cee87f4
Iulian <54375582+poenaruiulian@users.noreply.github.com>
2023-08-25 21:53:28 +0300

    Deleted the data for the firebaseConfig

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -6,13 +6,7 @@ import {getDatabase} from "@firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBOSD_FHR6cY_8VyTfXkB9l0VyZWrG0yi0",
-    authDomain: "collabconnect-dc39f.firebaseapp.com",
-    projectId: "collabconnect-dc39f",
-    storageBucket: "collabconnect-dc39f.appspot.com",
-    messagingSenderId: "18437694335",
-    appId: "1:18437694335:web:1904aed35016620a6f304a",
-    databaseURL:"https://collabconnect-dc39f-default-rtdb.firebaseio.com/"
+// firebase config details here
 };
 
 const app = initializeApp(firebaseConfig);
